84790088f0a3620be098a2ebe67bacbb5e1c112e
weiguoyuan <weiguoyuan@daojia.com>
2019-05-07 10:01:59 +0800

    test spi

--- src/main/java/testspi/TestSPI.java
+++ src/main/java/testspi/TestSPI.java
@@ -6,7 +6,7 @@ import java.util.ServiceLoader;
  * @author weiguoyuan 2019/5/6
  */
 public class TestSPI {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         ServiceLoader<Dog> loaders = ServiceLoader.load(Dog.class);
         for (Dog d : loaders){
             d.sleep();
